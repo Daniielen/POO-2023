bc3afc3fae80dd6a58ddcfb823877689657cabb9
Danielen <85040632+Daniielen@users.noreply.github.com>
2023-10-25 16:18:31 -0300

    :sparkles: [Tabelas] Criação, edição e remoção.

---
+++ [Lista 4] Interface Grafica/[Slide] Tabelas/src/slide/exemplo/caneta/Caneta.java
@@ -0,0 +1,47 @@
+package slide.exemplo.caneta;
+
+/**
+ *
+ * @author Danielen Santana
+ * @since 04/10/2023
+ * @details Criação da classe Caneta a partir do exemplo demonstrado no slide
+ * @note Exemplo criado no curso de Programação Orientada a Objeto 
+ * ministrada pelo professor Dr.Fabio Marques.
+ */
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    public String Escrever() {
+        return "Escrevendo...";
+    }
+    
+    public int tampar() {
+        int erro = 0;
+        if ( !tampada ) {
+            tampada = true;        
+        } else {
+            erro = 1;
+        }
+        return erro;
+    }
+    
+    public int destampar() {
+        int erro = 0;
+        if ( tampada ) {
+            tampada = false;        
+        } else {
+            erro = 1;
+        }
+        return erro;
+    }
+
+    @Override
+    public String toString() {
+        return "Caneta{" + "modelo=" + modelo + ", cor=" + cor + ", ponta=" + ponta + ", carga=" + carga + ", tampada=" + tampada + '}';
+    }
+    
+}
